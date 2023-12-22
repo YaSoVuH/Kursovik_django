a1e39881a8ae815b8fc4c289f86883b7b2aa2468
Ya_SoVuH <c_peshkov@mail.ru>
2023-12-22 19:51:31 +0300

    Change forms login and register

--- siteforkursovik/main/forms.py
+++ siteforkursovik/main/forms.py
@@ -3,27 +3,22 @@ from django.contrib.auth.models import User
 from django.forms import TextInput, PasswordInput, CharField
 
 class RegisterUserForm(UserCreationForm):
-
+    username = CharField(widget=TextInput(attrs={'placeholder': 'Никнейм'}))
+    password1 = CharField(widget=TextInput(attrs={'type': 'password', 'placeholder':'Пароль'}))
+    password2 = CharField(widget=TextInput(attrs={'type': 'password', 'placeholder':'Повторите пароль'}))
 
     class Meta:
         model = User
         fields = {'username', 'password1', 'password2'}
-        widgets = {
-            'username': TextInput(attrs={'class': 'form-input'}),
-            'password1': PasswordInput(attrs={'class': 'form-input', 'type': 'password'}),
-            'password2': PasswordInput(attrs={'class': 'form-input', 'type': 'password'}),
-        }
 
     field_order = ['username', 'password1', 'password2']
 
 class LoginUserForm(AuthenticationForm):
+    username = CharField(widget=TextInput(attrs={'placeholder': 'Никнейм'}))
+    password = CharField(widget=TextInput(attrs={'type': 'password', 'placeholder':'Пароль'}))
 
     class Meta:
         model = User
         fields = {'username', 'password'}
-        widgets = {
-            'username': TextInput(attrs={'class': 'form-input'}),
-            'password': PasswordInput(attrs={'class': 'form-input', 'type': 'password'}),
-        }
 
     field_order = ['username', 'password']
